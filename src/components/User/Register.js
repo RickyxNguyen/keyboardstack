90eb631d817feeffb835d1ee4139c5e06d31924d
Ricky <rickywolf28@yahoo.com>
2019-03-28 18:05:37 -0500

    commit

--- src/components/User/Register.js
+++ src/components/User/Register.js
@@ -171,7 +171,7 @@ class Register extends Component{
               )} />
               <div className="divider"/>
               <Route render={({history}) => (
-              <Button onClick={() => { history.push('/') }}>Return to Register</Button>
+              <Button onClick={() => { history.push('/') }}>Return to Home</Button>
               )} />
               <div className="divider"/>
               <Button color="primary"	type="submit">Register</Button>
